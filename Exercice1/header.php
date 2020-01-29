398d6f615580296dba27c3776c008b2b617afee6
Monvoisin <monvoisin.pierre@gmail.com>
2020-01-29 19:47:46 +0100

    Envoi des fichiers de base

---
+++ Exercice1/header.php
@@ -0,0 +1,7 @@
+<header>
+  <ul class="nav nav-pills nav-fill">
+    <li class="nav-item">
+      <a class="nav-link active" href="ajout-patient.php">Créer un compte</a>
+    </li>
+  </ul>
+</header>
