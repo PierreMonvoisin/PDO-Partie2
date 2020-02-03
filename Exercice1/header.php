3c11a03c3bf548f6aaa45b0d6ec2c8afe3cd3197
Monvoisin <monvoisin.pierre@gmail.com>
2020-02-03 09:52:09 +0100

    suppression des anciens dossiers

--- Exercice1/header.php
+++
@@ -1,7 +0,0 @@
-<header class="bg-light px-3">
-  <ul class="nav nav-pills nav-fill">
-    <li class="nav-item">
-      <a class="nav-link bg-dark active" href="ajout-patient.php">Créer un compte</a>
-    </li>
-  </ul>
-</header>
--- Exercice4/parameters.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Définition des constantes pour la connection à la base de données
-define('USER', 'pierremonvoisin');
-define('PASSWORD', 'monvoisin');
-define('HOST', 'localhost');
-define('DB', 'hospitalE2N');
